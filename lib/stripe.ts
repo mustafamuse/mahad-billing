4c2bcd34aad8bd04ccde18e8b0a531cd24f5f719
Mustafa Muse <umpp101@gmail.com>
2025-04-11 23:48:21 -0500

    upgrade stripe

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,28 +1,34 @@
 import Stripe from 'stripe'
-
 let stripeClient: Stripe | null = null
-
 // Get configured server-side Stripe client
 export function getStripeClient(): Stripe {
   if (!process.env.STRIPE_SECRET_KEY) {
     throw new Error(
-      'STRIPE_SECRET_KEY is not defined. Please add it to your environment variables.'
+      'STRIPE_SECRET_KEY is not defined. Please set it in your environment variables.'
     )
   }
 
   if (!stripeClient) {
     console.log('Initializing Stripe client...')
     stripeClient = new Stripe(process.env.STRIPE_SECRET_KEY, {
-      // apiVersion: '2025-02-24.acacia',
+      apiVersion: '2025-03-31.basil',
       typescript: true,
     })
   }
-
   return stripeClient
 }
 
 // Export a proxy object that lazily initializes the client
-export const stripeServerClient = getStripeClient()
+export const stripeServerClient = new Proxy({} as Stripe, {
+  get: (target, prop) => {
+    const client = getStripeClient()
+    const value = client[prop as keyof Stripe]
+    if (typeof value === 'function') {
+      return value.bind(client)
+    }
+    return value
+  },
+})
 
 // Optional: Test initialization during app startup
 export async function testStripeClientInitialization(): Promise<void> {
